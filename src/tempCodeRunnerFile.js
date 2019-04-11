30be1d687ef53189ca7eb5a5a056c1c59226a4a8
Gean-Santos <geansantos_15@hotmail.com>
2019-04-11 16:46:21 -0300

    Alteracao basica

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-
-mongoose.connect(
-    'mongodb+srv://geansantos:geansantos@cluster0-rlqes.azure.mongodb.net/bancoomnistack?retryWrites=true',
-{
-    useNewUrlParser:true
-}); 
+process.env.PORT 
